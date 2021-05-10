b95e07152bb3f71f1a184aea109e353944453794
Mateusz Brzozowski <01158596@pw.edu.pl>
2021-05-10 13:09:51 +0000

    Update cordic-lab1.asm

--- cordic-lab1.asm
+++ cordic-lab1.asm
@@ -1,8 +1,6 @@
 	.data
 	#Constant CORDIC value 1/K multiplied by scale
 CORDIC:	.word 0x26DD3B6A
-	#Scale to avoid non-integer constant variables 2^30
-SCALE:	.word 0x40000000
 	#Size of precomputed lookup table
 L_TABLE_SIZE: 	.word 32
 	#Precomputed lookup table
@@ -14,8 +12,8 @@ L_TABLE:	.word 0x3243F6A8, 0x1DAC6705, 0x0FADBAFC, 0x07F56EA6, 0x03FEAB76
 	.word 0x0000001F, 0x0000000F, 0x00000008, 0x00000004, 0x00000002
 	.word 0x00000001, 0x00000000
 
-PROMPT_INPUT:	.asciiz "Enter an angle between -pi/2 and pi/2, multiplied by 2^30\n"
-PROMPT_OUTPUT:	.asciiz "Results multiplied by 2^30:\n"
+PROMPT_INPUT:	.asciiz "Enter an angle between -1/2 and 1/2, multiplied by 2^8\n"
+PROMPT_OUTPUT:	.asciiz "Results multiplied by 2^8:\n"
 PORMPT_SIN:	.asciiz "sin = "
 PROMPT_COS:	.asciiz "\ncos = "
 
@@ -26,12 +24,13 @@ main:
 	la $a0, PROMPT_INPUT
 	syscall
 	
-	li $v0, 5		#int z = theta;
+	li $v0, 5		
 	syscall
 	
-	blt $v0, -1686629713, main
-	bgt $v0, 1686629713, main
+	blt $v0, -128, main
+	bgt $v0, 128, main
 cordic:
+	mul $t5, $v0, 13176794	#int z = theta [pi * rad];
 	lw $t0, CORDIC		#int x = CORDIC;
 	li $t1, 0		#int y = 0;
 	li $t2, 0		#int i = 0;
@@ -43,23 +42,23 @@ for:
 	srav $s1, $t0, $t2	#int ty = x >> i;
 	lw $s2, ($t3)		#int tz = L_TABLE[i];
 	
-	bgez $v0, else		#if (z >= 0)
+	bgez $t5, else		#if (z >= 0)
 	
 	add $t0, $t0, $s0	#x += tx;
 	sub $t1, $t1, $s1	#y -= ty;
-	add $v0, $v0, $s2	#z += tz;
+	add $t5, $t5, $s2	#z += tz;
 	b end_for		
 else:
 	sub $t0, $t0, $s0	#x -= tx;
 	add $t1, $t1, $s1	#y += ty;
-	sub $v0, $v0, $s2	#z -= tz;
+	sub $t5, $t5, $s2	#z -= tz;
 end_for:
 	addiu $t2, $t2, 1	#i++
 	addiu $t3, $t3, 4	#L_TABLE[i]++
 	
 	lw $s4, L_TABLE_SIZE
 	bne $t2, $s4, for	#~i < L_TABLE_SIZE
-	move $t4, $v0
+	move $t4, $t5
 	
 exit:
 	li $v0, 4
@@ -71,7 +70,7 @@ exit:
 	syscall
 		
 	li $v0, 1
-	move $a0, $t1
+	sra $a0, $t1, 22
 	syscall
 	
 	li $v0, 4
@@ -79,7 +78,7 @@ exit:
 	syscall
 		
 	li $v0, 1
-	move $a0, $t0
+	sra $a0, $t0, 22
 	syscall
 
 	li $v0, 10
